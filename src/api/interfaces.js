402fae5bce7f471880a88b83ff36b0dd289dc1b0
2839855598 <2839855598@qq.com>
2019-12-01 14:37:17 +0800

    single baseulr

--- src/api/interfaces.js
+++ src/api/interfaces.js
@@ -3,6 +3,6 @@
 const ports = {
   // 不能固定死，比如 http://localhost:8080/
   dev: '/',
-  production: 'http://47.105.169.196/food-mall/'
+  production: '/'
 }
 export default ports[process.env.NODE_ENV];
