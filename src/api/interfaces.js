e5db16a6c119e903168b2069db9a088e39867c28
2839855598 <2839855598@qq.com>
2019-12-01 15:34:34 +0800

    fix multip baseurl

--- src/api/interfaces.js
+++ src/api/interfaces.js
@@ -3,6 +3,6 @@
 const ports = {
   // 不能固定死，比如 http://localhost:8080/
   dev: '/',
-  production: '/'
+  production: 'http://47.105.169.196/food-mall/'
 }
 export default ports[process.env.NODE_ENV];
--- src/router.js
+++ src/router.js
@@ -9,7 +9,7 @@ Vue.use(Router)
 
 export default new Router({
   mode: 'history',
-  // base: process.env.NODE_ENV === 'production' ? '/food-mall/' : '/',
+  base: process.env.NODE_ENV === 'production' ? '/food-mall/' : '/',
   // 重置链接激活class类名
   linkActiveClass: 'active',
   routes: [
