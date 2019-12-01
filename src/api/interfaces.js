6010f544fe3f0bd70514cca07b2c22bb93bd9a66
2839855598 <2839855598@qq.com>
2019-12-01 13:51:53 +0800

    add baseurl

--- src/api/interfaces.js
+++ src/api/interfaces.js
@@ -3,6 +3,6 @@
 const ports = {
   // 不能固定死，比如 http://localhost:8080/
   dev: '/',
-  se: 'http://47.105.169.196/food-mall/'
+  production: 'http://47.105.169.196/food-mall/'
 }
 export default ports[process.env.NODE_ENV];
