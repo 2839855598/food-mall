8aa21b575aef5ac2a2d12858efe8f60beb4a0b36
2839855598 <2839855598@qq.com>
2019-12-01 19:42:33 +0800

    fix problems

--- src/api/interfaces.js
+++ src/api/interfaces.js
@@ -2,7 +2,9 @@
 // 管理域名接口
 const ports = {
   // 不能固定死，比如 http://localhost:8080/
-  dev: '/',
+  development: '/',
+  // 服务器有多个项目时候,production用具体的,只有单个项目时候(以/开头)
+  // production 用 /
   production: 'http://47.105.169.196/food-mall/'
 }
 export default ports[process.env.NODE_ENV];
